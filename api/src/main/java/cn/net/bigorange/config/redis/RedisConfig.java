837561e56dea6fbae263a8276b4e47c15f395a08
张鹏程 <zhangpengcheng@dong-xu.com>
2018-01-04 09:23:22 +0800

    add: Redis配置读取

---
+++ api/src/main/java/cn/net/bigorange/config/redis/RedisConfig.java
@@ -0,0 +1,22 @@
+package cn.net.bigorange.config.redis;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import java.lang.reflect.Method;
+
+/**
+ * Created by bigorange on 2018/1/2.
+ */
+@Configuration
+@ImportResource(locations = {"classpath:config/redis/spring-redis.xml"})
+public class RedisConfig extends CachingConfigurerSupport {
+
+}
