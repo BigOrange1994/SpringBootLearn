b048fdd9a8b0724cfb25de51780e4b5ffef71456
张鹏程 <zhangpengcheng@dong-xu.com>
2018-01-22 10:01:44 +0800

    add:Eureka 配置

---
+++ api/src/main/java/cn/net/bigorange/helper/eurekaserver/EurekaServiceApplication.java
@@ -0,0 +1,17 @@
+package cn.net.bigorange.helper.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by bigorange on 2018/1/15.
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServiceApplication.class, args);
+    }
+}
