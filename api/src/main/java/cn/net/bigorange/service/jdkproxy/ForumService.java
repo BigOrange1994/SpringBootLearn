9609f063d72cc8df9fc0d90db7ede636714b4c90
张鹏程 <zhangpengcheng@dong-xu.com>
2018-01-29 22:31:13 +0800

    add:需要CGlib动态代理的实现类的接口

---
+++ api/src/main/java/cn/net/bigorange/service/jdkproxy/ForumService.java
@@ -0,0 +1,12 @@
+package cn.net.bigorange.service.jdkproxy;
+
+/**
+ * Created by bigorange on 2018/1/24.
+ */
+public interface ForumService {
+
+    void removeTopic(int topicId);
+
+    void removeForum(int forumId);
+
+}
