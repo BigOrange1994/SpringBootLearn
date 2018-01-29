9b52e056991974054e7aec48619004542756e30b
张鹏程 <zhangpengcheng@dong-xu.com>
2018-01-29 22:27:18 +0800

    add:前置增强样例

---
+++ api/src/main/java/cn/net/bigorange/advice/GreetingBeforeAdvice.java
@@ -0,0 +1,18 @@
+package cn.net.bigorange.advice;
+
+import org.springframework.aop.MethodBeforeAdvice;
+
+import java.lang.reflect.Method;
+
+/**
+ * Created by bigorange on 2018/1/25.
+ */
+public class GreetingBeforeAdvice implements MethodBeforeAdvice {
+
+    @Override
+    public void before(Method method, Object[] args, Object target) throws Throwable {
+        String clientName = (String)args[0];
+        System.out.println("How are you " + clientName + ".");
+    }
+
+}
