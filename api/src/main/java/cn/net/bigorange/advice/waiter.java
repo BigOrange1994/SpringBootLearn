83cdce4ef68e3b61f69ebf2a2ed3dc3d5d94fa66
张鹏程 <zhangpengcheng@dong-xu.com>
2018-01-29 22:17:10 +0800

    add：wait服务者动作接口定义

---
+++ api/src/main/java/cn/net/bigorange/advice/waiter.java
@@ -0,0 +1,12 @@
+package cn.net.bigorange.advice;
+
+/**
+ * Created by bigorange on 2018/1/25.
+ */
+public interface Waiter {
+
+    void greetingTo(String name);
+
+    void serveTo(String name);
+
+}
