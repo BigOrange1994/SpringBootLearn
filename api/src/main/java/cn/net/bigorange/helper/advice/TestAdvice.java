c650b686b54b6c63b561b47299e4c178f38eaf24
张鹏程 <zhangpengcheng@dong-xu.com>
2018-01-30 23:13:41 +0800

    add:引介增强测试方法

--- api/src/main/java/cn/net/bigorange/helper/advice/TestAdvice.java
+++ api/src/main/java/cn/net/bigorange/helper/advice/TestAdvice.java
@@ -3,8 +3,12 @@ package cn.net.bigorange.helper.advice;
 import cn.net.bigorange.advice.GreetingBeforeAdvice;
 import cn.net.bigorange.advice.NativeWaiter;
 import cn.net.bigorange.advice.Waiter;
+import cn.net.bigorange.monitor.Monitorable;
+import cn.net.bigorange.service.jdkproxy.impl.ForumServiceImpl;
 import org.springframework.aop.BeforeAdvice;
 import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * Created by bigorange on 2018/1/25.
@@ -12,7 +16,8 @@ import org.springframework.aop.framework.ProxyFactory;
 public class TestAdvice {
 
     public static void main(String[] args) {
-        beforeAdvice();
+        //beforeAdvice();
+        interceptorAdvice();
     }
 
     public static void beforeAdvice(){
@@ -28,6 +33,18 @@ public class TestAdvice {
         Waiter proxyWaiter = (Waiter)pf.getProxy();
         proxyWaiter.greetingTo("Tom");
         proxyWaiter.serveTo("John");
+    }
+
+    public static void interceptorAdvice(){
+        String path = "config/advice/spring-advice.xml";
+        ApplicationContext ctx = new ClassPathXmlApplicationContext(path);
+        ForumServiceImpl impl = (ForumServiceImpl) ctx.getBean("forunService");
+        impl.removeTopic(1);
+        impl.removeForum(2);
+        Monitorable monitorable = (Monitorable) impl;
+        monitorable.setMonitorActice(true);
+        impl.removeTopic(3);
+        impl.removeForum(4);
     }
 
 }
