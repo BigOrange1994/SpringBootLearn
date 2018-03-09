adfd941834f3f6609affc04432b315b8ca83dbc1
张鹏程 <zhangpengcheng@dong-xu.com>
2018-03-09 15:18:37 +0800

    add：个推样例

---
+++ api/src/main/java/cn/net/bigorange/getui/GeTui.java
@@ -0,0 +1,23 @@
+package cn.net.bigorange.getui;
+
+import com.gexin.rp.sdk.base.impl.AppMessage;
+import com.gexin.rp.sdk.base.impl.Target;
+import com.gexin.rp.sdk.template.AbstractTemplate;
+import com.gexin.rp.sdk.template.LinkTemplate;
+import com.gexin.rp.sdk.template.NotificationTemplate;
+import com.gexin.rp.sdk.template.NotyPopLoadTemplate;
+
+/**
+ * Created by bigorange on 2018/2/5.
+ */
+public class GeTui {
+
+    public static void main(String[] args) {
+        AbstractTemplate template = null;
+        boolean a = template instanceof LinkTemplate;
+        NotyPopLoadTemplate b ;
+        AppMessage message = new AppMessage();
+        Target target = new Target();
+        NotificationTemplate template1 = new NotificationTemplate();
+    }
+}
